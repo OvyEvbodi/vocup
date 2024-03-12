f9300afba39b4913b1fe0e9570c69337ac29daa4
Ovy Evbodi <evbodiovo@gmail.com>
2024-03-13 00:15:43 +0100

    Implement signup

--- app.ts
+++ app.ts
@@ -8,18 +8,68 @@ const PORT = 8080;
 
 const database = new PrismaClient();
 
-app.get('/', (req, res) => {
+// express middleware
+app.use(express.json())
+
+app.get('/', (_, res) => {
   res.send('Welcome to PrisMongEx')
 })
 
-app.get('/database', (req, res) => {
+// sign up
+app.post('/signup', (req, res) => {
+  const saltRounds = 9;
+
+  // get user object
+  const { email, username, password } = req.body;
+
+  // validate all fields
+  // coming soon
+
+  // encrypt password
+  bcrypt.hash(password, saltRounds, (error, hash) => {
+    //handle errors
+    // coming soon
+
+     // create user in databse
+    const dbConnection = async () => {
+      await database.user.create({
+        data: {
+          username,
+          password: hash,
+          email
+        }
+      })
+    }
+    dbConnection()
+      .catch (async (error) => {
+        console.log("an error occured /n/n/n")
+        console.log(error)
+        process.exit(1)
+      })
+      .finally(async() => {
+        await database.$disconnect()
+      })
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Origin', '*');
+      res.setHeader('Content-Type', 'Application/json');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      res.status(201)
+      res.type('json')
+      res.send({ msg: "user created", email })
+      res.end()
+  })
+})
+
+
+app.get('/database', (_, res) => {
   async function db_connection() {
   const allUsers = await database.user.findMany();
   console.log(allUsers)
   return allUsers
 }
 
-    //convert to try catch bloc
+  //convert to try catch bloc
   db_connection()
     .then(users => {
       res.type('json')
