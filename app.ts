b7624313a82d494d5fe7c37f54c3c008cb79eedb
Ovy Evbodi <evbodiovo@gmail.com>
2024-03-12 22:38:40 +0100

    Set up express app

--- app.ts
+++ app.ts
@@ -1,19 +1,38 @@
 import { PrismaClient } from "@prisma/client"
+import express from 'express'
+
+const app = express();
+const PORT = 8080;
 
 const database = new PrismaClient();
 
-async function main() {
-    const allUsers = await database.user.findMany();
-    console.log(allUsers)
+app.get('/', (req, res) => {
+  res.send('Welcome to PrisMongEx')
+})
+
+app.get('/database', (req, res) => {
+  async function db_connection() {
+  const allUsers = await database.user.findMany();
+  console.log(allUsers)
+  return allUsers
 }
 
-//convert to try catch block
-main()
+    //convert to try catch bloc
+  db_connection()
+    .then(users => {
+      res.type('json')
+      res.send(JSON.stringify(users))
+    })
     .catch(async (error) => {
-        console.log("an error occured /n/n/n")
-        console.log(error)
-        process.exit(1)
+      console.log("an error occured /n/n/n")
+      console.log(error)
+      process.exit(1)
     })
     .finally(async() => {
-        await database.$disconnect()
+      await database.$disconnect()
     })
+})
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
